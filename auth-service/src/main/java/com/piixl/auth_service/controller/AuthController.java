0930ba09d5a8f33dd728b0791b381918e75b7d4b
khedermurad <khedermurad@outlook.com>
2026-06-29 18:15:56 +0200

    style(auth-service): fix checkstyle error

--- auth-service/src/main/java/com/piixl/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/piixl/auth_service/controller/AuthController.java
@@ -12,7 +12,14 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
 
 import java.util.Map;
 
