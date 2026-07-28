9b2d6b089d8fec7ccadc20cde97bafbff001aac2
khedermurad <khedermurad@outlook.com>
2026-07-28 18:49:35 +0200

    refactor(auth-service): add flyway baseline migration

---
+++ auth-service/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,25 @@
+CREATE DOMAIN email_address AS citext
+CHECK (
+    value ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+);
+
+CREATE TYPE user_role AS ENUM ('USER', 'ADMIN');
+
+CREATE TABLE users(
+id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+created_at DATE,
+email email_address NOT NULL,
+enabled BOOLEAN NOT NULL,
+password TEXT NOT NULL,
+role user_role NOT NULL DEFAULT 'USER',
+terms_accepted BOOLEAN,
+username TEXT NOT NULL,
+date_of_birth date NOT NULL,
+
+CONSTRAINT chk_terms_accepted CHECK (terms_accepted = true),
+CONSTRAINT chk_username_min_length CHECK (char_length(username) >= 5),
+CONSTRAINT chk_username_max_length CHECK (char_length(username) <= 20),
+CONSTRAINT chk_username_pattern CHECK (username ~ '^[A-Za-z]{5}.*'),
+CONSTRAINT uk_users_email UNIQUE(email),
+CONSTRAINT uk_users_username UNIQUE(username)
+);
