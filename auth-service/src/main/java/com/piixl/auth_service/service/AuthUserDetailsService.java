7076f91df6c2029058070c23c92cd18cbed4d9ae
khedermurad <khedermurad@outlook.com>
2026-08-05 18:58:35 +0200

    refactor(auth-service): replace specific repository class by abstract type for more flexibility

--- auth-service/src/main/java/com/piixl/auth_service/service/AuthUserDetailsService.java
+++ auth-service/src/main/java/com/piixl/auth_service/service/AuthUserDetailsService.java
@@ -2,6 +2,7 @@ package com.piixl.auth_service.service;
 
 import com.piixl.auth_service.model.UserEntity;
 import com.piixl.auth_service.repository.JdbcUserRepository;
+import com.piixl.auth_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,16 +12,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
-    private JdbcUserRepository jdbcUserRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public AuthUserDetailsService(JdbcUserRepository jdbcUserRepository){
-        this.jdbcUserRepository = jdbcUserRepository;
+    public AuthUserDetailsService(JdbcUserRepository userRepository){
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = jdbcUserRepository.findByUsername(username)
+        UserEntity userEntity = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
         return User
                 .withUsername(userEntity.getUsername())
